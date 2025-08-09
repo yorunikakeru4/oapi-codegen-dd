87a0e92b21fd6a5da368da4952a3c1c0ed87cf91
Igor Khitryk <igor.khitryk@wolt.com>
2025-08-09 17:19:08 +0200

    Change logger body type

--- pkg/runtime/logger.go
+++ pkg/runtime/logger.go
@@ -6,4 +6,4 @@ import (
 )
 
 // Logger is a function type that defines the signature for logging HTTP requests and responses.
-type Logger func(ctx context.Context, msg string, headers http.Header, body any, fields map[string]any)
+type Logger func(ctx context.Context, msg string, headers http.Header, body []byte, fields map[string]any)
